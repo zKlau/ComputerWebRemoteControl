aad32feb3da679b7f3c6dd5b474bd484e8c8f48a
zKlau <claudiupadure50@yahoo.com>
2021-12-19 04:43:57 +0200

    Mobile Support and new Layout

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var child = require('child_process').execFile;
 const chld = require('child_process');
 const shell = require('shelljs');
 const fs = require('fs');
+const useragent = require('express-useragent');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,6 +11,7 @@ const app = express();
 
 let del = false;
 const server = http.createServer(function(request, response) {
+  
   if (request.method == 'POST') {
     //console.log('POST')
     var body = ''
@@ -20,7 +22,11 @@ const server = http.createServer(function(request, response) {
       openProgram(body);
     })
   } else {
-    
+    // Detects if the user is on mobile or not
+    var source = request.headers['user-agent']
+    var ua = useragent.parse(source);
+    var isMobile = ua.isMobile
+
     //console.log('GET')
     response.writeHead(200, {'Content-Type': 'text/html'})
     const appText = fs.readFileSync("data.txt",'utf8');
@@ -29,21 +35,42 @@ const server = http.createServer(function(request, response) {
     const lines = appSplit.filter((value, index) => {
       return appSplit.indexOf(value) === index;
     });
+    lines.filter(n => n)
     response.write('<input id="add_name" style="display: inline-block;" type="text" placeholder="Name"> <br><form method="post" class="hide-mobile" > <input class="hidden" type="text" name="name" value="BrowseFile" /><input type="submit" value="BrowseFile" /> </form> <br>  <form method="post" class="hide-mobile"> <input class="hidden" type="text" name="name" value="Add" /><input type="submit" value="Add" onclick="deleteTimer()"/> </form>  <br> <form method="post" action=website> <input class="hidden" type="text" name="name" value="Delete"/><input type="submit" value="Delete" onclick="deleteTimer()"/> </form> <br>    <br><br>')
+    response.write("<table> <tr>")
+    var lineNumber = 0;
+
+
     for(let i = 0 ; i < lines.length ; i++) {
       if(lines[i] != "") {
+        lineNumber++;
+
+        //Adds the shortcut to the current line number
+        response.write("<td>")
         let apps = lines[i].split("@")
-        response.write('<form method="post" > <input class="hidden" type="text" name="name" value=' + lines[i] + ' /><input type="submit" value=' + apps[0] + ' /> </form>');
+        response.write('<form method="post" > <input class="hidden" type="text" name="name" value=' + lines[i] + ' /><input id="formPost" type="submit" value=' + apps[0] + ' /> </form>');
+        response.write("</td>")
+
+        // If the user is on mobile the shortcuts will be arranged in different ways (default : 10 mobile and 30 pc)
+        var res = isMobile ? lines.length/100 * 10 : lines.length/100 * 30;
+
+        if(lineNumber >= res)
+        {
+          //Closes a line and starts a new one
+          response.write("</tr><tr>")
+          lineNumber = 0;
+        }
       }
-      
     }
+    //Ends the table
+    response.write("</table>")
     fs.createReadStream('index.html').pipe(response);
   }
 });
 
 function openProgram(name)
 {
-  //console.log(decodeURIComponent(name));
+  
   let og = name;
   name = decodeURIComponent(name);
   if(name == "name=BrowseFile")
@@ -62,7 +89,7 @@ function openProgram(name)
     SaveApps(app[1],path);
     
   }
-  //console.log(name);
+
   if(name == "name=Delete") 
   {
     del = true;
@@ -74,7 +101,6 @@ function openProgram(name)
     let app = name.replace("name=", "").split("@");
     let path = app[1];
     path = path.replaceAll("^","^ ");
-    //console.log(path);
     chld.exec(path);
   }
 
@@ -87,10 +113,10 @@ function openProgram(name)
     const appText = fs.readFileSync("data.txt",'utf8');
     const appSplit = appText.split(/\r?\n/);
     let removeApp = [];
-    // the array contains every shortcut from 'data.txt' without the one that the user selected
+
+    // The array contains every shortcut from 'data.txt' without the one that the user selected
     let newApps = appSplit.filter(item => item !== app[0] + "@" + path);
     removeApp = newApps;
-    //console.log(app[0] + "@" + path);
 
    newApps.forEach(element => {
      console.log(element);
