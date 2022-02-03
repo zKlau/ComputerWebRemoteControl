be67d7cbbfc962b123efe1c760fa26b88859f970
zKlau <claudiupadure50@yahoo.com>
2022-02-03 07:38:58 +0200

    test

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const host = "localhost";
 
 http.createServer(function(req,res) {
     res.writeHead(200,{'Content-Type': 'text/html'});
-    //res.write('<button href="app.runAse()">click</button>');
-    //res.end('<input id = "Double Click here" type = "button" value = "clickme" onclick = "console.log("working");" />');
     fs.createReadStream('index.html').pipe(res)
 }).listen(port, host);
 console.log("Server on!");
-
-
-
-// lt --port 3000
